fd0ff79b956db891f2c5925239c51035bca9c6c1
gomezmig03 <gomezmig03@gmail.com>
2024-08-24 18:13:15 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## How to develop locally
+After cloning the branch, install the needed dependencies
+```
+npm install
+```
+
+Then you can test it
+```
+npm run dev
+```
+
+And build it into a dist directory
+```
+npm run build
+```
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <img src="../assets/dd-frontend-256x256.png" id="disk-icon" />
+  <h1>
+    DD Frontend
+  </h1>
+  <h3>
+    A GUI for a very useful utility.
+  </h3>
+
+</template>
+
+<style >
+</style>
