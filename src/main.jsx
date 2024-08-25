64943ff0ed341a2fe640a189e80d437d1014a825
gomezmig03 <gomezmig03@gmail.com>
2024-08-25 22:31:38 +0200

    Update createRoot and fix token bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import App from './App'
 import { init } from '@neutralinojs/lib'
@@ -9,11 +9,11 @@ import { init } from '@neutralinojs/lib'
 // }
 // Neutralino.events.on("windowClose", onWindowClose);
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
 
 init();
