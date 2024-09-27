a11550d884f3aa71aacf13f2842e7cbd864b1848
gomezmig03 <gomezmig03@gmail.com>
2024-09-27 13:53:52 +0200

    Make lists close when clicking outside of them

--- src/components/DiskList.jsx
+++ src/components/DiskList.jsx
@@ -1,13 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { getDiskROM } from "../utilities/commands"
 import { useDDstore } from "../store"
 
 const DiskList = (props) => {
+    const ref = useRef(null)
+
     const [openList, setOpenList] = useState(false)
     const [diskList, setDiskList] = useState([])
     const setInput = useDDstore((s) => s.setInput)
     const setOutput = useDDstore((s) => s.setOutput)
     const disk = useDDstore((s) => s.disk)
+
+    const outsideClick = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setOpenList(false)
+        }
+    }
+
+    document.addEventListener('mousedown', outsideClick)
 
     useEffect(() => {
         getDiskROM(disk)
@@ -24,7 +34,7 @@ const DiskList = (props) => {
     }
 
     return(
-        <div className="disk-list">
+        <div className="disk-list" ref={ref} >
             <img src="icons/down-arrow-svgrepo.svg" onClick={() => setOpenList(!openList)} />
             {openList && (
                 <ul className="disk-rom-list">
